15f76bc25be44f432908946e2c845f2729b1f4e7
Dustmire <Dustmire@proton.me>
2026-02-01 01:07:12 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,38 @@
+import re
+import subprocess
+
+p = subprocess.Popen(
+    ["/readflag"],   
+    stdin=subprocess.PIPE,
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE,
+    universal_newlines=True
+)
+
+expr = None
+while True:
+    line = p.stdout.readline()
+    if not line:
+        break
+    line = line.strip()
+    print("题目行:", line)
+    
+    # 匹配数字+加减括号
+    m = re.search(r'[-+()0-9]+', line)
+    if m:
+        expr = m.group()
+        break
+
+if expr is None:
+    raise ValueError("没有找到算术表达式！")
+
+# 计算结果
+result = eval(expr)
+print("答案:", result)
+
+# 发回程序
+p.stdin.write(str(result) + "\n")
+p.stdin.flush()
+
+# 打印剩余输出
+print(p.stdout.read())
